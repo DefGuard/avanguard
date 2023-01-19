5a1a8ad6c59dfb1eec42d4467e6324f589fd7c6f
Jacek Chmielewski <jchmielewski@teonite.com>
2023-01-19 08:26:06 +0000

    Setup database with sqlx library

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -3,9 +3,9 @@ use log::LevelFilter;
 
 #[derive(Clone, Parser)]
 pub struct Config {
-    #[clap(long, env = "VG_LISTEN_PORT", default_value_t = 8081)]
+    #[clap(long, env = "AG_LISTEN_PORT", default_value_t = 8081)]
     pub listen_port: u16,
 
-    #[clap(long, env = "VG_LOG_LEVEL", default_value_t = LevelFilter::Info)]
+    #[clap(long, env = "AG_LOG_LEVEL", default_value_t = LevelFilter::Info)]
     pub log_level: LevelFilter,
 }
